180dd7d7eb0430f90705c5c233e5d4f5c383898a
myHQS <huoqingshuai@163.com>
2019-03-23 10:07:03 +0800

    第一次提交

---
+++ src/com/testGit/FirstGit.java
@@ -0,0 +1,13 @@
+package com.testGit;
+
+/**
+ * @ClassName FirstGit
+ * @Date 2019/3/23 9:53
+ */
+public class FirstGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
