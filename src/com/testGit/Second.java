d7e1e6d42160c32611b0b18ed32a34d543d8d9f9
myHQS <huoqingshuai@163.com>
2019-03-23 10:25:59 +0800

    dev分支  添加second类

---
+++ src/com/testGit/Second.java
@@ -0,0 +1,11 @@
+package com.testGit;
+
+/**
+ * @ClassName Second
+ * @Date 2019/3/23 10:25
+ */
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("second");
+    }
+}
